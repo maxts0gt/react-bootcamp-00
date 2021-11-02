e43045309ad45471e3535b724904f9c16ccb39d4
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-11-02 14:58:50 +0900

    add navbar and finishing touch

--- 06-widgets/src/App.js
+++ 06-widgets/src/App.js
@@ -1,13 +1,59 @@
-import React from 'react';
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import Dropdown from './components/Dropdown';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+
+const items = [
+	{
+		title: 'What is React?',
+		content: 'It is JS framework',
+	},
+	{
+		title: 'Why use React?',
+		content: 'It makes web dynamic',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'By working your ass off!',
+	},
+];
+
+const options = [
+	{
+		label: 'The Color Red',
+		value: 'red',
+	},
+	{
+		label: 'The Color Blue',
+		value: 'blue',
+	},
+	{
+		label: 'The Color Green',
+		value: 'green',
+	},
+];
 
 const App = () => {
+	const [selected, setSelected] = useState(options[0]);
+
 	return (
 		<div>
-			<Translate />
+			<Header />
+			<Route path='/'>
+				<Accordion items={items} />
+			</Route>
+			<Route path='/list'>
+				<Search />
+			</Route>
+			<Route path='/dropdown'>
+				<Dropdown label='Select a color' options={options} selected={selected} onSelectedChange={setSelected} />
+			</Route>
+			<Route path='/translate'>
+				<Translate />
+			</Route>
 		</div>
 	);
 };
