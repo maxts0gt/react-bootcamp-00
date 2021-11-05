55532bea6b55381008ff7f12e29c41299ba43b6a
Max <bazarsadtsogtjargal@gmail.com>
2021-11-05 23:16:22 +0900

    set up streams and finish nav part

---
+++ 10-streams/client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+const App = () => {
+	return (
+		<div className='ui container'>
+			<Router>
+				<Header />
+				<Routes>
+					<Route path='/' exact element={<StreamList />} />
+					<Route path='/streams/new' exact element={<StreamCreate />} />
+					<Route path='/streams/edit' exact element={<StreamEdit />} />
+					<Route path='/streams/delete' exact element={<StreamDelete />} />
+					<Route path='/streams/show' exact element={<StreamShow />} />
+				</Routes>
+			</Router>
+		</div>
+	);
+};
+
+export default App;
