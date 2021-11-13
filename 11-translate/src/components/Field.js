2e22bb2b37e86dcdcfd7d202bce42e756ff0bfab
Max <bazarsadtsogtjargal@gmail.com>
2021-11-13 10:42:54 +0900

    implementing context

--- 11-translate/src/components/Field.js
+++ 11-translate/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
 	static contextType = LanguageContext;
 
 	render() {
-		const text = this.context === 'english' ? 'Name' : 'Нэр';
+		const text = this.context.language === 'english' ? 'Name' : 'Нэр';
 		return (
 			<div className='ui field'>
 				<label htmlFor=''>{text}</label>
---
+++ 11-translate/src/components/LanguageSelector.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+export default class LanguageSelector extends React.Component {
+	static contextType = LanguageContext;
+	render() {
+		return (
+			<div>
+				Select a language:
+				<i
+					className='flag us'
+					onClick={() => this.context.onLanguageChange('english')}
+				/>
+				<i
+					className='flag mn'
+					onClick={() => this.context.onLanguageChange('mongolian')}
+				/>
+			</div>
+		);
+	}
+}
